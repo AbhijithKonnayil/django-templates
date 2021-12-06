2426b82651de4fae34b09d242b2defe43efc7436
Abhijith K <iamabhijith.k@gmail.com>
2021-12-07 03:57:00 +0530

    created rest template

---
+++ rest_template/factory.py
@@ -0,0 +1,5 @@
+import factory
+from factory.django import DjangoModelFactory
+from .models import *
+
+#create your factory here
---
+++ rest_template/serializers.py
@@ -0,0 +1,6 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import *
+
+
+#create your serializer here
